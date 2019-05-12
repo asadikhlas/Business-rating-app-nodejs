9e1ba6fe39c9a41dab609c8d29c548141a944e04
Asad Ikhlas <asadikhlas03@gmail.com>
2019-05-12 23:02:08 +0500

    working

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
+const engine = require("ejs-mate");
+const session = require("express-session");
+const app = express();
 
-app.listen(3000, ()=> {
-    console.log('Listening on port 3000')
-})
+app.use(express.static("public"));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
