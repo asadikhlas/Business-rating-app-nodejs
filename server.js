62b56a733613652a094f57d2cec4afa5ea2f8de2
Asad Ikhlas <asadikhlas03@gmail.com>
2019-05-12 23:17:09 +0500

    working

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const cookieParser = require("cookie-parser");
 const ejs = require("ejs");
 const engine = require("ejs-mate");
 const session = require("express-session");
+const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')(session)
 const app = express();
 
+mongoose.connect('mongodb://localhost/rateme')
 app.use(express.static("public"));
 app.engine("ejs", engine);
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(session({
+    secret:'Thisismysecretkey',
+    resave:false,
+    saveUninitialized:false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
 
 app.get("/", (req, res, next) => {
   res.render("index");
